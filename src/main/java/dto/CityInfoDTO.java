dc94c31dc6185c8ee1b575a66695a28f2f5150cc
Cahit <cahit10@hotmail.com>
2020-03-15 16:16:10 +0100

    Made cityinfoDTO

---
+++ src/main/java/dto/CityInfoDTO.java
@@ -0,0 +1,61 @@
+package dto;
+
+import entities.Address;
+import entities.CityInfo;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author cahit
+ */
+public class CityInfoDTO {
+
+    private String zipCode;
+    private String city;
+    private List<String> addresses = new ArrayList();
+
+    public CityInfoDTO() {
+    }
+
+    public CityInfoDTO(String zipCode, String city, List<Address> addresses) {
+        this.zipCode = zipCode;
+        this.city = city;
+        for (Address addresse : addresses) {
+            this.addresses.add(addresse.getStreet());
+        }
+    }
+
+    public CityInfoDTO(CityInfo city) {
+        this.zipCode = city.getZipCode();
+        this.city = city.getCity();
+        for (Address addresse : city.getAddresses()) {
+            this.addresses.add(addresse.getStreet());
+        }
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public List<String> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<String> addresses) {
+        this.addresses = addresses;
+    }
+
+}
