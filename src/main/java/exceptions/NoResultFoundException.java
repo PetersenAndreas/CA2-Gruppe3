3d8f306dc5ec0ec9bf5c241888ad3490d74d7f3f
Michael N. Korsgaard <Michael12.korsgaard@gmail.com>
2020-03-17 10:44:49 +0100

    added Exceptions

---
+++ src/main/java/exceptions/NoResultFoundException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class NoResultFoundException extends Exception {
+    public NoResultFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/rest/HobbiesResource.java
+++ src/main/java/rest/HobbiesResource.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
 import com.google.gson.Gson;
@@ -19,11 +14,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
 import utils.EMF_Creator;
 
-/**
- * REST Web Service
- *
- * @author Bruger
- */
 @Path("hobbies")
 public class HobbiesResource {
 
