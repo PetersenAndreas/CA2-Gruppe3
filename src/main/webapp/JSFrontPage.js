b493c3e0863c44336a0073ff0543f9280f4bbd73
Andreas <andreas.petersen.3540@gmail.com>
2020-03-20 14:25:22 +0100

    You can now add a new person via post!

--- src/main/webapp/JSFrontPage.js
+++ src/main/webapp/JSFrontPage.js
@@ -4,8 +4,11 @@
  * and open the template in the editor.
  */
 fetchAllPersons();
+fetchHobbies();
+fetchStreet();
 
-let personHobbies = [];
+let newHobbies = [];
+let newPhones = [];
 
 function clickSearchID(evt) {
     evt.preventDefault();
@@ -26,16 +29,17 @@ function reloadeUsers(evt) {
 }
 
 document.getElementById("rldAllBtn").addEventListener("click", reloadeUsers);
+document.getElementById("addHobby").addEventListener("click", addHobby);
+document.getElementById("addPhone").addEventListener("click", addPhone);
 
 function fetchAllPersons() {
     let url = "api/persons/";
-    let header = "<tr>" + "<th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th>" + "</tr>";
+    let header = "<tr>" + "<th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone(s)</th><th>Description</th><th>Hobby</th><th>City</th><th>Street</th><th>Zip</th>" + "</tr>";
     fetch(url)
             .then(res => res.json())
             .then(data => {
                 console.log("data", data);
-                console.log(data);
-                document.getElementById("myDiv").innerHTML = "<table border='1'>" + header + insertIntoTableFooters(data.persons) + "</table>";
+                document.getElementById("myDiv").innerHTML = "<table border='1' style='width: 80%;'>" + header + insertIntoTableFooters(data.persons) + "</table>";
             });
 }
 
@@ -47,6 +51,12 @@ function insertIntoTableFooters(data) {
                 + "<td>" + e.firstName + "</td>"
                 + "<td>" + e.lastName + "</td>"
                 + "<td>" + e.email + "</td>"
+                + "<td>" + e.phones.phones.map(x => x.number).join("<br>") + "</td>"
+                + "<td>" + e.phones.phones.map(x => x.description).join("<br>") + "</td>"
+                + "<td>" + e.hobbies.join("<br>") + "</td>"
+                + "<td>" + e.cityName + "</td>"
+                + "<td>" + e.street + "</td>"
+                + "<td>" + e.zip + "</td>"
                 + "</tr>";
         htmlRows += temp;
     });
@@ -58,7 +68,7 @@ function insertIntoTableFooters(data) {
 function fetchPersonOnId() {
     let searchId = document.getElementById("searchIdPer").value;
     let url = "api/persons/" + searchId;
-    let header = "<tr>" + "<th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th>" + "</tr>";
+    let header = "<tr>" + "<th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Phone(s)</th><th>Description</th><th>Hobby</th><th>City</th><th>Street</th><th>Zip</th>" + "</tr>";
     fetch(url)
             .then(res => res.json())
             .then(data => {
@@ -69,58 +79,128 @@ function fetchPersonOnId() {
                     fetchAllPersons();
                     document.getElementById("error").innerHTML = data.message;
                 } else {
-                    document.getElementById("myDiv").innerHTML = "<table border='1'>" + header + insertIntoTableFooters([data]) + "</table>";
+                    document.getElementById("myDiv").innerHTML = "<table border='1' style='width: 80%;'>" + header + insertIntoTableFooters([data]) + "</table>";
                 }
             });
 }
 document.getElementById("searchIdBtn").addEventListener("click", clickSearchID);
 
 
+function clearError() {
+    document.getElementById("error").innerHTML = "<br>";
+}
 
-function loadeCreate() {
+function addPhone() {
+    let phone = {
+        number: document.getElementById("inputPhoneNrC").value,
+        description: document.getElementById("inputPhoneDesC").value};
+    newPhones.push(phone);
+    console.log(newPhones);
+    document.getElementById("addedPhones").innerHTML = newPhones.map(x=>x.number + ", " + x.description).join("<br>");
+    document.getElementById("inputPhoneNrC").value = "";
+    document.getElementById("inputPhoneDesC").value = "";
+}
+
+function addHobby() {
+    let hobby = document.getElementById("inputHobbiesC").value;
+    newHobbies.push(hobby);
+    console.log(newHobbies);
+    document.getElementById("addedHobbies").innerHTML = newHobbies.join("<br>");
     
-    let url = "api/hobbies/"; 
+    
+}
+
+function createPerson() {
+    let firstName = document.getElementById("inputFirstNameC").value;
+    let lastName = document.getElementById("inputLastNameC").value;
+    let email = document.getElementById("inputEmailC").value;
+
+    
+    let street = document.getElementById("inputStreetC").value;
+    let options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            phones: {phones: newPhones},
+            hobbies: newHobbies,
+            street: street
+        })
+    };
+    fetch("api/persons/add", options)
+            .then(res => res.json())
+            .then(data => {
+                console.log("data", data);
+            });
+    newPhones = [];
+    document.getElementById("addedPhones").innerHTML = newPhones.map(x=>x.number + ", " + x.description).join("<br>");
+    newHobbies = [];
+    document.getElementById("addedHobbies").innerHTML = newHobbies.join("<br>");
+}
+document.getElementById("createPersonBtn").addEventListener("click", createPerson);
+
+function fetchHobbies() {
+    let url = "api/hobbies/";
     fetch(url)
             .then(res => res.json())
             .then(data => {
-            document.getElementById("myDiv").innerHTML = 
-            "<input type='text' name='txt' value='' id='firstName' placeholder='First Name' style='width: 150px'/>" + "  First Name"
-            + '<br>' + "<input type='text' name='txt' value=''id='lastName' placeholder='Last Name'/ style='width: 150px'>" + "  Last Name"
-            + '<br>' + "<input type='text' name='txt' value=''id='email' placeholder='Email' style='width: 150px'/>" + "  Email"
-            + '<br>' + "<input type='text' name='txt' value=''id='phone' placeholder='Phone' style='width: 150px'/>" + "  Phone"
-            + '<br>' + createPersonHobbyTable()
-            + "<p id='disAddedHobby'></p>"
-            + '<br>' + "<button id='addBtn'>ADD Hobby to Person</button>"
-            + '<br>' + "<button id='createbtn'>Create</button>";
-            
-    });
-    document.getElementById("addBtn").addEventListener("click", addHobbyToArray);
-    personHobbies = [];
-    
-    
-   
-
+                console.log("data", data);
+                document.getElementById("inputHobbiesC").innerHTML = "<option>" + data.hobbies.map(x=>x.name).join("</option><option>") + "</option>";
+            });
 }
 
-function addHobbyToArray() {
-    personHobbies[document.getElementById("hobby").text];
-}
-
-document.getElementById("createSelection").addEventListener("click", create);
-
-function createPersonHobbyTable() {
-    let headers = "<table><tr><th>Hobby Name</th><th>Description</th><th>Select</th></tr></table>";
-    
+function fetchStreet() {
+    let url = "api/addresses/";
+    fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log("data", data);
+                document.getElementById("inputStreetC").innerHTML = "<option>" + data.addressList.map(x=>x.street).join("</option><option>") + "</option>";
+            });
 }
 
 
 
 
-
-function clearError() {
-    document.getElementById("error").innerHTML ="<br>";
-}
-
-
-
+//Graveyard
+//function loadeCreate() {
+//    
+//    let url = "api/hobbies/"; 
+//    fetch(url)
+//            .then(res => res.json())
+//            .then(data => {
+//            document.getElementById("myDiv").innerHTML = 
+//            "<input type='text' name='txt' value='' id='firstName' placeholder='First Name' style='width: 150px'/>" + "  First Name"
+//            + '<br>' + "<input type='text' name='txt' value=''id='lastName' placeholder='Last Name'/ style='width: 150px'>" + "  Last Name"
+//            + '<br>' + "<input type='text' name='txt' value=''id='email' placeholder='Email' style='width: 150px'/>" + "  Email"
+//            + '<br>' + "<input type='text' name='txt' value=''id='phone' placeholder='Phone' style='width: 150px'/>" + "  Phone"
+//            + '<br>' + createPersonHobbyTable()
+//            + "<p id='disAddedHobby'></p>"
+//            + '<br>' + "<button id='addBtn'>ADD Hobby to Person</button>"
+//            + '<br>' + "<button id='createbtn'>Create</button>";
+//            
+//    });
+//    document.getElementById("addBtn").addEventListener("click", addHobbyToArray);
+//    personHobbies = [];
+//    
+//    
+//   
+//
+//}
+//
+//function addHobbyToArray() {
+//    personHobbies[document.getElementById("hobby").text];
+//}
+//
+//document.getElementById("createSelection").addEventListener("click", create);
+//
+//function createPersonHobbyTable() {
+//    let headers = "<table><tr><th>Hobby Name</th><th>Description</th><th>Select</th></tr></table>";
+//    
+//}
 
